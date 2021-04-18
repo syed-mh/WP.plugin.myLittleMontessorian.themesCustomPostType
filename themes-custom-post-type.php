3dce1f03c3668a0e95f35836c6e13d944a5a4f9d
Syed MH <hassan@syedmh.com>
2021-04-18 23:49:37 +0500

    Initial commit ;

---
+++ themes-custom-post-type.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @package ThemesCustomPostType
+ * @author Syed Mohammed Hassan <mohammad.hassan@va8ivedigital.com>
+ * @copyright 2021 Va8ive Digital
+ * @license GPL-2.0-or-later
+ * 
+ * Plugin Name: Themes Custom Post Type
+ * Description: This is a custom plugin developed for Shaping Nutrition that adds a custom post type with the name "Themes". This plugin requires the free or PRO version of ACF to be installed.
+ * Version: 0.0.1
+ * Author: Syed Mohammed Hassan
+ * Author URI: https://va8ivedigital.com/
+ * Text Domain: themes=custom-post-type
+ */
+
+/*
+ * Themes Custom Post Type is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ * 
+ * Themes Custom Post Type is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+*/
+
+/**
+ * Security Measure
+ */
+if(!defined('ABSPATH')) exit;
